916f6d515c6a6f4ff20b208ac8ffa250f2d4e740
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-23 21:07:22 +0530

    test updates

--- test/blocktron-lib.spec.js
+++ test/blocktron-lib.spec.js
@@ -7,13 +7,27 @@
 const blocktron = require('../src/index');
 const blockchain = new blocktron;
 
+/**
+ * Empty chain test 1
+ */
 test('Given an empty instance of blocktron-lib', () => {
     expect(blockchain).not.toBeNull();
 });
 
+/**
+ * Empty chain test 2
+ */
 test('Given an empty instance of blocktron-lib', () => {
     expect(blockchain).toEqual({
         chain: [],
         newTransactions: []
     });
+});
+
+/**
+ * Create a block - test
+ */
+test('Try adding data to block', () => {
+    blockchain.createNewBlock(2389, 'a87sdfs8df8ds89f', '23huih2342jh34j');
+    expect(blockchain).not.toBeNull();
 });
