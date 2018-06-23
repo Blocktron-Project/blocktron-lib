2616196a6a5a4e8933589af110df40aa55b02839
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-23 13:35:48 +0530

    newBlock test spec added

--- test/blocktron-lib.spec.js
+++ test/blocktron-lib.spec.js
@@ -3,3 +3,17 @@
  * This spec file contains the test specs for blocktron-lib
  * Written by: Sandeep Vattapparambil
  */
+
+const blocktron = require('../src/index');
+const blockchain = new blocktron;
+
+test('Given an empty instance of blocktron-lib', () => {
+    expect(blockchain).not.toBeNull();
+});
+
+test('Given an empty instance of blocktron-lib', () => {
+    expect(blockchain).toEqual({
+        chain: [],
+        newTransactions: []
+    });
+});
