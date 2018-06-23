5bc0d29eed6e356d9349283bbf07cd387e912160
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-23 22:12:59 +0530

    test updates

--- test/blocktron-lib.spec.js
+++ test/blocktron-lib.spec.js
@@ -39,3 +39,12 @@ test('Test 4: Create a new transaction and add to pending transactions', () => {
     blockchain.createNewTransaction(100, 'ROSS2IU3Y42U3', 'RACHELJ234J234KJ20');
     expect(blockchain).not.toBeNull();
 });
+
+/**
+ * Mine the block after creating the block
+ */
+test('Test 5: Mine the block back to the transactions after creating the block', () => {
+    blockchain.createNewBlock(1289, 'a87sdhkf8ds89f', '23huigf9fg67h34j');
+    expect(blockchain).not.toBeNull();
+    expect(blockchain.chain[1]).not.toBeNull();
+});
